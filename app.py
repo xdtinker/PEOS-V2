a780e475c1090eb667c8bbd8538027086237f218
xdtinker <93765440+xdtinker@users.noreply.github.com>
2022-11-03 19:05:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ app.url_map.strict_slashes = False
 def home():
     return telegram
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    #app.debug = True
+    app.run(host="0.0.0.0",port= 5000)
