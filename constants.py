0bf8a42ba9710eccedb8a0f655e47ef0c65abcd9
xdtinker <93765440+xdtinker@users.noreply.github.com>
2022-11-09 17:34:39 +0800

    Delete constants.py

--- constants.py
+++
@@ -1,8 +0,0 @@
-_url = 'https://peos.dmw.gov.ph/controller/login.php'
-_logout = 'https://peos.dmw.gov.ph/logout.php'
-_patch_url = 'https://peos.dmw.gov.ph/hhw.php'
-_module_url = 'https://peos.dmw.gov.ph/controller/selectQuestionsHHW.php'
-_check_answer = 'https://peos.dmw.gov.ph/controller/checkAnswerHHW.php'
-_get_cert = 'https://peos.dmw.gov.ph/controller/selectModuleHHW.php'
-_fileLink = 'https://peos.dmw.gov.ph/peoshhwpdf.php'
-API_TOKEN = os.environ['API_TOKEN']
